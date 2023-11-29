c07a2e503bcb7c6a50b83a653859d263f61dcd45
mattstam <mattstam@live.com>
2023-11-29 14:52:21 -0800

    cleanup

--- convert_verifier.sh
+++ convert_verifier.sh
@@ -12,14 +12,10 @@ interface IFunctionVerifier {
 }
 
 contract FunctionVerifier is IFunctionVerifier, UltraVerifier {
-    event VerifyInputs(bytes32 input0, bytes32 input1, bytes proof);
-
     function verify(bytes32 _inputHash, bytes32 _outputHash, bytes memory _proof) external returns (bool) {
         bytes32[] memory input = new bytes32[](2);
         input[0] = bytes32(uint256(_inputHash) & ((1 << 253) - 1));
         input[1] = bytes32(uint256(_outputHash) & ((1 << 253) - 1));
-
-        emit VerifyInputs(input[0], input[1], _proof);
 
         return this.verify(_proof, input);
     }
